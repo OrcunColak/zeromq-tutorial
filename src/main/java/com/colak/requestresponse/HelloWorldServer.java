8aa7ec7a3e1ba91ab4e66fdbc51f4e2bd54c4667
Orçun Çolak <orcun.colak@hazelcast.com>
2025-02-24 06:52:23 +0300

    Initial commit

---
+++ src/main/java/com/colak/requestresponse/HelloWorldServer.java
@@ -0,0 +1,29 @@
+package com.colak.requestresponse;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class HelloWorldServer {
+
+    public static void main() {
+
+        try (ZContext context = new ZContext()) {
+            // Socket to talk to clients
+            ZMQ.Socket socket = context.createSocket(SocketType.REP);
+            socket.bind("tcp://*:5555");
+
+            while (!Thread.currentThread().isInterrupted()) {
+                // Block until a message is received
+                byte[] reply = socket.recv(0);
+
+                // Print the message
+                System.out.println("Received: [" + new String(reply, ZMQ.CHARSET) + "]");
+
+                // Send a response
+                String response = "Hello, world!";
+                socket.send(response.getBytes(ZMQ.CHARSET), 0);
+            }
+        }
+    }
+}
